b5b98257782237dccd39b9ccd953628280b9a8d1
katya-petrova <walki-disi@ya.ru>
2021-02-08 15:39:42 +0300

    feat: add functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (typeof(array) == 'undefined' || array.length === 0) {
+        return 0;
+   }
+return Math.min.apply(null,array);
 }
 
 exports.max = function max (array) {
-  return 0;
+    if (typeof(array) == 'undefined' || array.length === 0) {
+        return 0;
+   }
+return Math.max.apply(null,array);
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if (typeof(array) == 'undefined' || array.length === 0) {
+        return 0;
+   }
+    var result = array.reduce(function(sum, current) {
+        return sum + current;
+      }, 0);
+      return result / array.length;
+      
 }
